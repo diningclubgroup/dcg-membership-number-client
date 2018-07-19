5a3e1040f93b224ea8bac99c3e31106af799f05c
gulzaman.khan <gulzaman.khan@tastecard.co.uk>
2018-07-19 14:58:31 +0100

    AT-575 moved config to separate package

--- config.php
+++ config.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    // The membership number api URL
 	'api_base_url' => 'https://member-num.test.diningclubgroup.com/v1',
+
+    // The authentication token for accessing the API (brand specific, e.g. Tastecard or Gourmet)
     'api_access_token' => 'TEST_TOKEN'
 ];
