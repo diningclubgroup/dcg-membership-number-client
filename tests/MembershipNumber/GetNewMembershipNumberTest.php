89bf416aee402234b011581508bc58859995ad5b
Dave Richards <dave.richards@tastecard.co.uk>
2019-06-24 15:32:01 +0100

    QRC-43: Upgrades GuzzleHttp to ^6.3

--- tests/MembershipNumber/GetNewMembershipNumberTest.php
+++ tests/MembershipNumber/GetNewMembershipNumberTest.php
@@ -2,10 +2,11 @@
 
 use Dcg\Client\MembershipNumber\Client;
 use Dcg\Client\MembershipNumber\Config;
-use GuzzleHttp\Message\Response;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Response;
 use GuzzleHttp\Stream\Stream;
-use GuzzleHttp\Subscriber\History;
-use GuzzleHttp\Subscriber\Mock;
 use PHPUnit\Framework\TestCase;
 
 class GetNewMembershipNumberTest extends TestCase
@@ -26,33 +27,32 @@ class GetNewMembershipNumberTest extends TestCase
      */
     public function does_client_return_membership_number()
     {
-        $mock = new Mock([
+        $mock = new MockHandler([
             new Response(200, [], Stream::factory(json_encode(['membership_number' => '1234567'])))
         ]);
+        $handler = HandlerStack::create($mock);
 
-        $client = new Client([], $this->testConfig);
-
-        $client->getEmitter()->attach($mock);
+        $client = new Client(['handler' => $handler], $this->testConfig);
 
         $this->assertEquals('1234567', $client->getNewMembershipNumber());
     }
 
     public function does_client_set_access_token_header()
     {
-        $mock = new Mock([
+        $mock = new MockHandler([
             new Response(200, [], Stream::factory(json_encode(['membership_number' => '1234567'])))
         ]);
+        $handler = HandlerStack::create($mock);
 
-        $history = new History();
+        $container = [];
+        $history = Middleware::history($container);
+        $handler->push($history);
 
-        $client = new Client([], $this->testConfig);
-
-        $client->getEmitter()->attach($mock);
-        $client->getEmitter()->attach($history);
+        $client = new Client(['handler' => $handler], $this->testConfig);
 
         $client->getNewMembershipNumber();
 
-        $lastRequest = $history->getLastRequest();
+        $lastRequest = end($container);
 
         $this->assertEquals('TEST_TOKEN', $lastRequest->getHeader('Access-Token'));
     }
@@ -62,15 +62,17 @@ class GetNewMembershipNumberTest extends TestCase
      */
     public function does_client_handle_404_error()
     {
-        $mock = new Mock([
+        $mock = new MockHandler([
             new Response(404, [], Stream::factory(json_encode(['error' => 'Unable to allocate membership number'])))
         ]);
+        $handler = HandlerStack::create($mock);
 
-        $client = new Client([], $this->testConfig);
+        $client = new Client(['handler' => $handler], $this->testConfig);
 
-        $client->getEmitter()->attach($mock);
-
-        $this->setExpectedException('\\Dcg\\Client\\MembershipNumber\\Exception\\MembershipNumberException', 'Unable to allocate membership number');
+        $this->setExpectedException(
+            '\\Dcg\\Client\\MembershipNumber\\Exception\\MembershipNumberException',
+            'Unable to allocate membership number'
+        );
 
         $client->getNewMembershipNumber();
     }
@@ -80,13 +82,12 @@ class GetNewMembershipNumberTest extends TestCase
      */
     public function does_client_handle_500_error()
     {
-        $mock = new Mock([
+        $mock = new MockHandler([
             new Response(500)
         ]);
+        $handler = HandlerStack::create($mock);
 
-        $client = new Client([], $this->testConfig);
-
-        $client->getEmitter()->attach($mock);
+        $client = new Client(['handler' => $handler], $this->testConfig);
 
         $this->setExpectedException('\\Dcg\\Client\\MembershipNumber\\Exception\\MembershipNumberException');
 
